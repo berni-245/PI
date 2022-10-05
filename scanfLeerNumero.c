b077bdc65e5e53137f0b7f65735c45bbdb41ad83
berni-245 <90289132+berni-245@users.noreply.github.com>
2022-10-05 17:53:54 -0300

    Add files via upload

---
+++ scanfLeerNumero.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+#define BORRA_BUFFER while (getchar() != '\n')
+#define NUM_CANT 3
+
+void
+leerNumero(int numFinal[]){
+    char numero[NUM_CANT+1];
+    char cond[6];
+    int i, coerencia, cant, digito;
+
+    sprintf(cond,"%%%ds", NUM_CANT); // NUM_CANT = 5 -> %5s
+
+    printf("Ingrese su respuesta (cantidad de digitos= %d): ",NUM_CANT);
+    do{
+        coerencia = true;
+        cant = scanf(cond, numero);
+        BORRA_BUFFER;
+
+        if(strlen(numero) != NUM_CANT)
+            coerencia = false;
+
+        for(i=0;isdigit(numero[i]) && i<'\0';i++){
+            numFinal[i] = numero[i]-'0';
+        }
+        if(i == NUM_CANT)
+            coerencia = true;
+
+    }while(coerencia == false && cant != 1);
+}
