0d3d6a55aae9d86fdb4e15a8f35390bf256bb5af
berni-245 <90289132+berni-245@users.noreply.github.com>
2022-10-05 18:14:09 -0300

    Update scanfLeerNumero.c

--- scanfLeerNumero.c
+++ scanfLeerNumero.c
@@ -6,26 +6,26 @@ void
 leerNumero(int numFinal[]){
     char numero[NUM_CANT+1];
     char cond[6];
-    int i, coerencia, cant, digito;
+    int i, coherencia, cant, digito;
 
     sprintf(cond,"%%%ds", NUM_CANT); // NUM_CANT = 5 -> %5s
 
     printf("Ingrese su respuesta (cantidad de digitos= %d): ",NUM_CANT);
     do{
-        coerencia = true;
+        coherencia = true;
         cant = scanf(cond, numero);
         BORRA_BUFFER;
 
         if(strlen(numero) != NUM_CANT)
-            coerencia = false;
+            coherencia = false;
 
         for(i=0;isdigit(numero[i]) && numero[i]!='\0';i++){
             numFinal[i] = numero[i]-'0';
         }
         if(i == NUM_CANT)
-            coerencia = true;
+            coherencia = true;
 
-    }while(coerencia == false && cant != 1);
+    }while(coherencia == false && cant != 1);
 }
 
 //conclusión: los scand son raros
