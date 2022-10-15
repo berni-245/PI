93bcc0d5dc579aa42460f7403b80e29393b69dc5
berni-245 <90289132+berni-245@users.noreply.github.com>
2022-10-15 19:14:52 -0300

    Update G8E4a.c

--- G8E4a.c
+++ G8E4a.c
@@ -1,3 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CARTAS_MAZO 52
+#define CANT_FIGURAS 13
+#define CANT_PALOS 4
+#define CARTAS_JUEGO 5
+#define LETRAS_PALO 8
+enum posNumeros{AZ=1,DOS,TRES,CUATRO,CINCO,SEIS,SIETE,OCHO,NUEVE,DIEZ,J,Q,K};
+
+typedef struct{
+    int numero;
+    char palo[LETRAS_PALO+1];  
+}carta;
+
+typedef carta cartas[CARTAS_MAZO];
+typedef carta * jugador; 
+
+void generarMazo(cartas mazo);
+void mezclarMazo(cartas mazo);
+jugador * repartirCartas(cartas mazo, int * dim);
+void checkearMano(jugador jugadorX);
+void imprimirMano(jugador jugadorX);
+
+int
+main(){
+    cartas mazo;
+    jugador jugador1;
+    int dim = CARTAS_MAZO;
+
+    generarMazo(mazo);
+
+    do{
+        jugador1 = repartirCartas(mazo, &dim);
+        checkearMano(jugador1);
+        imprimirMano(jugador1);
+
+    }while(dim > 0);
+    printf("FIN.\n");
+
+
+    return 0;
+}
+
 void
 generarMazo(cartas mazo){
     int i, j;
