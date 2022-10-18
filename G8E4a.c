4e912125c03ef544f4a511f49b633c268ef3ee56
berni-245 <90289132+berni-245@users.noreply.github.com>
2022-10-17 21:13:30 -0300

    Update G8E4a.c

--- G8E4a.c
+++ G8E4a.c
@@ -155,7 +155,7 @@ checkearMano(jugador jugadorX){
 
 /*
 Dudas:
-1) Está bien el algoritmo para generar el mazo? O debería contemplar que CANT_PALOS y CANT_FIGURAS no sean los típicos del mazo francés?
+1) Está bien el algoritmo para generar el mazo? O debería contemplar que CANT_PALOS y CANT_FIGURAS no sean los típicos del mazo francés? (4 palos y 13 figuras)
 2) Está bien la estructura general del programa? Me inquieta un poco que todas las funciones me retornaran void jaja
 3) La última función quedó medio "rara", hay alguna forma más eficiente de plantearla?
 */
